03d510994f9d1a35470f5801bcfb210adf9018eb
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 14:42:00 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //倒星形三角形
+            for(int i = 0; i <= 5; i++)
+            {
+                int A = 5 - i ;
+                string row = new string('*', A);
+                Console.WriteLine(row);
+            }
+
+
+        }
+    }
+}
